70b3cb9ae99bdd88e27b8a8a9e18967ab1bdddd2
magdacislerova <55711011+magdacislerova@users.noreply.github.com>
2020-05-11 11:29:13 +0200

    Vylepšení

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ console.log('funguju!');
 
 const tasks = ['Vyprat ponožky', 'Naučit se funkce vyššího řádu', 'Nakoupit na víkend', 'Upéct dort', 'Udělat prezentaci na pokrok v dizertační práci'];
 
-const toDoList = document.querySelector('.todo__tasks');
-
 const updateTasks = () => {
+  const toDoList = document.querySelector('.todo__tasks');
+  toDoList.innerHTML = '';
   for (let i = 0; i < tasks.length; i += 1) {
     toDoList.innerHTML += `<div class = "task"> ${tasks[i]} </div>`;
   };
@@ -17,8 +17,10 @@ updateTasks();
 
 const btnElm = document.querySelector('.btn-add');
 const inputElm = document.querySelector('.new-task');
+
 btnElm.addEventListener('click', () => {
   tasks.push(inputElm.value);
-  toDoList.innerHTML = '';
   updateTasks();
-});
+  inputElm.value = '';
+}
+);
